3d864edb75ff3636b7be863e661b13bac8a0fa72
Dewansh <dhruugupta@gmail.com>
2025-09-12 15:55:24 +0530

    Done

---
+++ src/main/java/in/expedia/rest/MsgRestController.java
@@ -0,0 +1,21 @@
+package in.expedia.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MsgRestController {
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		String msg = "Welcome to REST API..!!";
+		return msg;
+	}
+
+	@GetMapping("/greet")
+	public String getGreetMsg(@RequestParam("name") String name) {
+		String msg = name + ", Good Morning...!!";
+		return msg;
+	}
+
+}
