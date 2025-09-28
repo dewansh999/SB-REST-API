b665ef03b34e120061d844fdb8b8ebfaf72f5d57
Dewansh Gupta <82531138+dewansh999@users.noreply.github.com>
2025-09-28 21:32:20 +0530

    Update MsgRestController.java

--- src/main/java/in/expedia/rest/MsgRestController.java
+++ src/main/java/in/expedia/rest/MsgRestController.java
@@ -1,7 +1,5 @@
 package in.expedia.rest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
